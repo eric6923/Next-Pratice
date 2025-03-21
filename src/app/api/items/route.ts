6c707400e584a548c286ba59cf894bdc5dde96ad
ERIC <eric.2002@srit.org>
2025-03-21 15:06:55 +0530

    POST Body Check

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,10 +1,14 @@
-export async function GET(){
+export async function GET(req:any){
+    const type = req.nextUrl.searchParams.get('type')
+    console.log("Type: ",type)
     return Response.json([
         {'title':'Nokia Mobile'},
         {'title':'Samsung Mobile'}
     ])
 }
 
-export async function POST(){
+export async function POST(req:any){
+    const body = await req.json()
+    console.log(body)
     return Response.json({message:"Message sent for POST"})
 }
